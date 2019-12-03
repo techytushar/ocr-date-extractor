44d0ca9a834f6f797cca959d9f3588885cf75dcb
Tushar Mittal <chiragmittal.mittal@gmail.com>
2019-12-03 20:13:10 +0530

    Add new API routes for Base64 encoded image

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from base64 import b64decode
+from werkzeug.datastructures import FileStorage
+from extract_date import find_date
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class UploadImage(Resource):
+class SendB64Image(Resource):
     def post(self):
         parse = reqparse.RequestParser()
-        parse.add_argument('base_64_image_content', type=bytes)
+        parse.add_argument('base_64_image_content', type=str)
         args = parse.parse_args()
         b64_img = args['base_64_image_content']
-        with open('img.jpeg', 'wb') as f:
+        with open('./static/uploaded_images/img.jpeg', 'wb') as f:
             f.write(b64decode(b64_img)) 
         return {"upload":"success"}
 
-api.add_resource(UploadImage, '/extract_date')
+class UploadImage(Resource):
+    def post(self):
+        parse = reqparse.RequestParser()
+        parse.add_argument('image', type=FileStorage, location='files')
+        args = parse.parse_args()
+        img = args['image']
+        img.save("./static/uploaded_images/img.jpg")
+
+api.add_resource(SendB64Image, '/extract_date')
+api.add_resource(UploadImage, '/extract_date_from_image')
---
+++ extract_date.py
@@ -0,0 +1,4 @@
+from utils import rescale_image, threshold, crop_img, find_bbox, remove_noise_and_smooth
+
+def find_date(img):
+    return None
